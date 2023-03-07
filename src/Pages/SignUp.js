6169fc8f3d86f6714b4ccf26b6be62a35ff1cb07
Sean Yuan <ysean54@yahoo.com>
2023-03-06 21:52:48 -0500

    relative added to back arrow

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -44,7 +44,7 @@ const SignUp = () => {
         <img className="absolute w-full h-full object-cover mix-blend-overlay" src={Medications} alt="" />
         <div className="flex justify-center items-center h-full">
             <form className="max-w-[400px] w-full mx-auto bg-white p-8">
-            <BiArrowBack onClick={() => back(-1)} className="cursor-pointer text-[20px]" />
+            <BiArrowBack onClick={() => back(-1)} className="relative cursor-pointer text-[20px]" />
                 <h2 className="text-4xl font-bold text-center py-8 relative">PharmRx Sign Up</h2>
                 <div className="flex flex-col mb-4">
                     <label className="relative mb-2">Email:</label>
